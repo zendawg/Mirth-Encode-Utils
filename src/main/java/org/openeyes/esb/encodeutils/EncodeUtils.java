ef5c168d41a00dde5c8399d4dcc768a0e2fb859c
zendawg <rich@statefive.org>
2013-03-25 16:20:32 +0000

    Renamed packages appropriately.

--- src/main/java/org/openeyes/esb/encodeutils/EncodeUtils.java
+++ src/main/java/org/openeyes/esb/encodeutils/EncodeUtils.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package org.statefive.encodeutils;
+package org.openeyes.esb.encodeutils;
 
 /**
  * MD5 encoding utilities.
--- src/test/java/org/openeyes/esb/encodeutils/EncodeUtilsTest.java
+++ src/test/java/org/openeyes/esb/encodeutils/EncodeUtilsTest.java
@@ -2,8 +2,9 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package org.statefive.encodeutils;
+package org.openeyes.esb.encodeutils;
 
+import org.openeyes.esb.encodeutils.EncodeUtils;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
